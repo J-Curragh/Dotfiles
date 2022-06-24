eb16675c5df77a12a1043b1e5a90b93bc1549fdc
J-Curragh <jcurragh03@gmail.com>
2022-06-24 19:49:50 +0100

    feat(Neovim): Telescope configuration

--- Neovim/.config/nvim/lua/jcli/telescope.lua
+++ Neovim/.config/nvim/lua/jcli/telescope.lua
@@ -1,28 +1,59 @@
+local previewers = require("telescope.previewers")
 local actions = require("telescope.actions")
+
+local Job = require("plenary.job")
 
 local M = {}
 
 require("telescope").setup({
-    defaults = {
-        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-
-	    mappings = {
-            i = {
-                ["<C-s>"] = actions.cycle_previewers_next,
-                ["<C-a>"] = actions.cycle_previewers_prev,
-                ["<esc>"] = actions.close,
-            },
-        },
+  defaults = {
+    border = {},
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    entry_prefix = " ",
+    file_ignore_patterns = {},
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    initial_mode = "insert",
+    layout_strategy = "horizontal",
+    mappings = {
+      i = {
+        ["<C-s>"] = actions.cycle_previewers_next,
+        ["<C-a>"] = actions.cycle_previewers_prev,
+        ["<esc>"] = actions.close,
+      },
     },
-    extensions = {
-        project = {
-            base_dirs = {
-                "~/Projects"
-            }
-        },
-        hidden_files = true,
-        theme = "dropdown"
-    }
+    path_display = { shorten = 5 },
+    pickers = {
+      find_files = { hidden = true },
+      live_grep = { only_sort_text = true },
+    },
+    prompt_prefix = " ",
+    selection_caret = " ",
+    selection_strategy = "reset",
+    set_env = { ["COLORTERM"] = "truecolor" },
+    sorting_strategy = "ascending",
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--glob=!.git/",
+    },
+    winblend = 0,
+  },
+  extensions = {
+    project = {
+        base_dirs = {
+            "~/Projects",
+        }
+    },
+    hidden_files = true,
+    theme = "dropdown"
+  }
 })
 
 
@@ -32,9 +63,9 @@ require("telescope").load_extension("project")
 
 -- Custom functions
 M.find_all_files = function()
-    require("telescope.builtin").find_files({
-        hidden = true
-    })
+  local opts = {}
+  local ok = pcall(require("telescope.builtin").git_files, opts)
+  if not ok then require("telescope.builtin").find_files(opts) end
 end
 
 M.search_from_home = function()
